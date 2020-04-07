eb05391e7a488d285695acad6b1f3da4730d1854
Jon <jdcorn@live.com>
2020-04-07 10:24:02 -0600

    add starter project files

---
+++ AutoPopulateMap/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AutoPopulateMap
+//
+//  Created by Jon Corn on 4/7/20.
+//  Copyright © 2020 Jon Corn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
